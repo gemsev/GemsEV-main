b37ea6663a9ac26c04cc3af94c3dbaed1bb0b824
gemsev <gemsevbharat@gmail.com>
2026-05-07 12:22:41 +0530

    Switch to frontend-only Vercel config

--- api/index.ts
+++
@@ -1,7 +0,0 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-import app from "../artifacts/api-server/src/app";
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  return app(req, res);
-}
-
